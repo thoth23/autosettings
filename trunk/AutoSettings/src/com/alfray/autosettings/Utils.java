eb138365bd905329aa97a1d925893ad9f491be87
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2008-12-08 05:56:35 +0000

    Set alarms. Format dates. Scroll log view.

---
+++ trunk/AutoSettings/src/com/alfray/autosettings/Utils.java
@@ -0,0 +1,54 @@
+/*
+ * (c) ralfoide gmail com, 2008
+ * Project: AutoSettings
+ * License TBD
+ */
+
+
+package com.alfray.autosettings;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+public class Utils {
+
+    public static CharSequence formatTime(Object caller, long time_ms, long now) {
+        /* The following method is not visible anymore in SDK 1.0 but still present,
+           just use reflection to do the equivalent of:
+            
+        CharSequence mod = android.pim.DateUtils.getRelativeTimeSpanString(
+                time_ms,
+                now,
+                0); // minresolution
+        */
+        
+        ClassLoader cl = caller.getClass().getClassLoader();
+        Class<?> du_class;
+        try {
+            du_class = cl.loadClass("android.pim.DateUtils");
+            Method method = du_class.getDeclaredMethod("getRelativeTimeSpanString",
+                    new Class[] { long.class, long.class, long.class });
+            Object v = method.invoke(null /*receiver*/, new Object[] { time_ms, now, (long)0 });
+
+            if (v instanceof CharSequence) {
+                return (CharSequence) v;
+            }
+        } catch (ClassNotFoundException e) {
+            // pass
+        } catch (SecurityException e) {
+            // pass
+        } catch (NoSuchMethodException e) {
+            // pass
+        } catch (IllegalArgumentException e) {
+            // pass
+        } catch (IllegalAccessException e) {
+            // pass
+        } catch (InvocationTargetException e) {
+            // pass
+        }
+        
+        return null;
+    }
+    
+}
