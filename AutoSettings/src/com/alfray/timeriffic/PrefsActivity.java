097cad2902076be095a81edc10e22afea7fcf22d
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-01-20 02:55:51 +0000

    Version 0.05: TimePicker preference. Changed internals to change on hour:minute (not just hour).

--- AutoSettings/src/com/alfray/timeriffic/PrefsActivity.java
+++ AutoSettings/src/com/alfray/timeriffic/PrefsActivity.java
@@ -7,7 +7,9 @@
 package com.alfray.timeriffic;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.view.WindowManager;
 
 /**
@@ -15,7 +17,7 @@ import android.view.WindowManager;
  */
 public class PrefsActivity extends PreferenceActivity {
 
-	public static final String EXTRA_ENABLE_VISUALS = "enable-visuals";
+    private PrefsValues mPrefValues;
 
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +29,48 @@ public class PrefsActivity extends PreferenceActivity {
 
 		setTitle(R.string.prefs_title);
 		addPreferencesFromResource(R.xml.prefs);
+
+		mPrefValues = new PrefsValues(this);
+		
+		Preference p = findPreference(PrefsValues.KEY_START_HOUR);
+		if (p != null) {
+		    p.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference pref, Object newValue) {
+                    setHourMinLabel(pref, newValue);
+                    return true;
+                }
+		    });
+		    setHourMinLabel(p, null);
+		}
+
+        p = findPreference(PrefsValues.KEY_END_HOUR);
+        if (p != null) {
+            p.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference pref, Object newValue) {
+                    setHourMinLabel(pref, newValue);
+                    return true;
+                }
+            });
+            setHourMinLabel(p, null);
+        }
 	}
+
+    protected void setHourMinLabel(Preference pref, Object value) {
+        int hourMin = 0;
+        if (value instanceof Integer) {
+            hourMin = ((Integer) value).intValue();
+        } else if (pref.getKey().equals(PrefsValues.KEY_START_HOUR)) {
+            hourMin = mPrefValues.startHourMin();
+        } else if (pref.getKey().equals(PrefsValues.KEY_END_HOUR)) {
+            hourMin = mPrefValues.stopHourMin();
+        } else {
+            // nothing applicable, give up
+            pref.setSummary("--");
+            return;
+        }
+
+        pref.setSummary(TimePreference.toHourMinStr(hourMin));
+    }
 }
