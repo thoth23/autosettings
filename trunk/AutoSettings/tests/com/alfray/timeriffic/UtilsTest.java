93aa047d5f50a0201830672c6dabd706605c200c
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-01-20 21:34:37 +0000

    Remove obsolete code

--- trunk/AutoSettings/tests/com/alfray/timeriffic/UtilsTest.java
+++ trunk/AutoSettings/tests/com/alfray/timeriffic/UtilsTest.java
@@ -35,10 +35,6 @@ public class UtilsTest {
         return mSf.parse(time).getTime();
     }
 
-    private void assertNotEquals(String expected, String actual) {
-        assertFalse(expected.equals(actual));
-    }
-
     // ----
     
     @Test
