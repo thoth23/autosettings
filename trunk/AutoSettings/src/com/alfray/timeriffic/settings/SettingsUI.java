194187d63b99cf8f7cad99b2c7ce0e3ac96ee118
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-01-21 08:05:29 +0000

    Experimenting with settings list

---
+++ trunk/AutoSettings/src/com/alfray/timeriffic/settings/SettingsUI.java
@@ -0,0 +1,35 @@
+/*
+ * (c) ralfoide gmail com, 2009
+ * Project: Timeriffic
+ * License TBD
+ */
+
+package com.alfray.timeriffic.settings;
+
+import com.alfray.timeriffic.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class SettingsUI extends Activity {
+
+    private LinearLayout mProfilesLinear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.settings_screen);
+        
+        mProfilesLinear = (LinearLayout) findViewById(R.id.ProfilesLinear);
+        addDummy();
+        addDummy();
+    }
+
+    private void addDummy() {
+        Profile p = new Profile();
+        p.addTo(getLayoutInflater(), mProfilesLinear);
+    }
+    
+}
