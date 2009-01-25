41176222871c64a142cc8dd71e94103321a0121e
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-01-25 07:37:13 +0000

    task list, comments code

--- trunk/AutoSettings/src/com/alfray/timeriffic/profiles/ProfilesUI.java
+++ trunk/AutoSettings/src/com/alfray/timeriffic/profiles/ProfilesUI.java
@@ -6,6 +6,8 @@
 
 package com.alfray.timeriffic.profiles;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
@@ -21,6 +23,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+import android.widget.AbsListView.RecyclerListener;
 
 import com.alfray.timeriffic.R;
 import com.alfray.timeriffic.prefs.PrefsValues;
@@ -39,6 +42,12 @@ public class ProfilesUI extends Activity {
     private Drawable mGreenDot;
     private Drawable mGrayDot;
 
+    /**
+     * Called when the activity is created.
+     * <p/>
+     * Initializes row indexes and buttons.
+     * Profile list & db is initialized in {@link #onResume()}.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,12 +59,42 @@ public class ProfilesUI extends Activity {
         mGreenDot = getResources().getDrawable(R.drawable.green_dot);
         mGrayDot = getResources().getDrawable(R.drawable.gray_dot);
         
-        initProfileList();
         initButtons();
     }
 
+    /**
+     * Called when activity is resume, or just after creation.
+     * <p/>
+     * Initializes the profile list & db.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initProfileList();
+    }
+
+    /**
+     * Called when the activity is getting paused. It might get destroyed
+     * at any point.
+     * <p/>
+     * Reclaim all views (so that they tag's cursor can be cleared).
+     * Destroys the db connection.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ArrayList<View> views = new ArrayList<View>();
+        mProfilesList.reclaimViews(views);
+        mProfilesDb.onDestroy();
+    }
+
+    /**
+     * Initializes the profile list widget with a cursor adapter.
+     * Creates a db connection.
+     */
     private void initProfileList() {
         mProfilesList = (ListView) findViewById(R.id.profilesList);
+        mProfilesList.setRecyclerListener(new ProfileRecyclerListener());
         
         mProfilesDb = new ProfilesDB();
         mProfilesDb.onCreate(this);
@@ -85,6 +124,9 @@ public class ProfilesUI extends Activity {
         mProfilesList.setAdapter(mAdapter);
     }
 
+    /**
+     * Initializes the list-independent buttons: global toggle, check now.
+     */
     private void initButtons() {
         final ToggleButton globalToggle = (ToggleButton) findViewById(R.id.global_toggle);
 
@@ -104,31 +146,65 @@ public class ProfilesUI extends Activity {
 
     //--------------
 
+    /**
+     * A custom {@link CursorAdapter} that can provide the two views we
+     * need: the profile header and the timed action entry.
+     * <p/>
+     * For each new view, the tag is set to either {@link ProfileHeaderHolder}
+     * or {@link TimedActionHolder}, a subclass of {@link CursorHolder}.
+     * <p/>
+     * When a view is reused, it's tag is reused with a new cursor by using
+     * {@link CursorHolder#setUiData(Cursor)}. This also updates the view
+     * with the data from the cursor.
+     * <p/>
+     * When a view is recycled/reclaimed, it's tag is cleared by the
+     * {@link ProfileRecyclerListener}.
+     */
     private class ProfileCursorAdapter extends CursorAdapter {
 
+        /** View type is a profile header. */
         private final static int TYPE_PROFILE = 0;
+        /** View type is a timed action item. */
         private final static int TYPE_TIMED_ACTION = 1;
         
 
-        public ProfileCursorAdapter(Context context, Cursor c) {
-            super(context, c);
+        /**
+         * Creates a new {@link ProfileCursorAdapter} for that cursor
+         * and context.
+         */
+        public ProfileCursorAdapter(Context context, Cursor cursor) {
+            super(context, cursor);
         }
         
+        /**
+         * All items are always enabled in this view.
+         */
         @Override
         public boolean areAllItemsEnabled() {
             return true;
         }
 
+        /**
+         * All items are always enabled in this view.
+         */
         @Override
         public boolean isEnabled(int position) {
             return true;
         }
-        
+
+        /**
+         * This adapter can serve 2 view types.
+         */
         @Override
         public int getViewTypeCount() {
             return 2;
         }
 
+        /**
+         * View types served are either {@link #TYPE_PROFILE} or
+         * {@link #TYPE_TIMED_ACTION}. This is based on the value of
+         * {@link Columns#TYPE} in the cursor.
+         */
         @Override
         public int getItemViewType(int position) {
             Cursor c = (Cursor) getItem(position);
@@ -143,38 +219,47 @@ public class ProfilesUI extends Activity {
 
         // ---
 
+        /**
+         * Depending on the value of {@link Columns#TYPE} in the cursor,
+         * this inflates either a profile_header or a timed_action resource.
+         * <p/>
+         * It then associates the tag with a new {@link ProfileHeaderHolder}
+         * or {@link TimedActionHolder} and initializes the holder using
+         * {@link CursorHolder#setUiData(Cursor)}.
+         * 
+         */
         @Override
         public View newView(Context context, Cursor cursor, ViewGroup parent) {
 
+            View v = null;
+            CursorHolder h = null;
+            
             int type = cursor.getInt(mTypeColIndex);
             if (type == Columns.TYPE_IS_PROFILE) {
-                View v = mLayoutInflater.inflate(R.layout.profile_header, null);
-                ProfileHeaderHolder h = new ProfileHeaderHolder(cursor, v);
+                v = mLayoutInflater.inflate(R.layout.profile_header, null);
+                h = new ProfileHeaderHolder(cursor, v);
+            } else if (type == Columns.TYPE_IS_TIMED_ACTION) {
+                v = mLayoutInflater.inflate(R.layout.timed_action, null);
+                h = new TimedActionHolder(cursor, v);
+            }
+            if (v != null) {
                 v.setTag(h);
                 h.setUiData(cursor);
-                return v;
             }
-
-            if (type == Columns.TYPE_IS_TIMED_ACTION) {
-                View v = mLayoutInflater.inflate(R.layout.timed_action, null);
-                TimedActionHolder h = new TimedActionHolder(cursor, v);
-                v.setTag(h);
-                h.setUiData(cursor);
-                return v;
-            }
-
-            return null;
+            return v;
         }
         
+        /**
+         * To recycle a view, we just re-associate its tag using
+         * {@link CursorHolder#setUiData(Cursor)}.
+         */
         @Override
         public void bindView(View view, Context context, Cursor cursor) {
 
             int type = cursor.getInt(mTypeColIndex);
-            if (type == Columns.TYPE_IS_PROFILE) {
-                ProfileHeaderHolder h = (ProfileHeaderHolder) view.getTag();
-                h.setUiData(cursor);
-            } else if (type == Columns.TYPE_IS_TIMED_ACTION) {
-                TimedActionHolder h = ( TimedActionHolder) view.getTag();
+            if (type == Columns.TYPE_IS_PROFILE ||
+                    type == Columns.TYPE_IS_TIMED_ACTION) {
+                CursorHolder h = (CursorHolder) view.getTag();
                 h.setUiData(cursor);
             }
         }
@@ -182,23 +267,63 @@ public class ProfilesUI extends Activity {
     
     //--------------
 
+    /**
+     * This {@link RecyclerListener} is attached to the profile list to
+     * call {@link CursorHolder#clearCursor()} of the tags of the reclaimed
+     * views. This should ensure that not dangling cursor reference exists.
+     */
+    private class ProfileRecyclerListener implements RecyclerListener {
+        @Override
+        public void onMovedToScrapHeap(View view) {
+            Object tag = view.getTag();
+            if (tag instanceof CursorHolder) {
+                ((CursorHolder) tag).clearCursor(); 
+            }
+        }
+    }
 
-    private class ProfileHeaderHolder {
+    /**
+     * A base holder class that keeps tracks of the current cursor.
+     */
+    private abstract class CursorHolder {
+        
+        /** The current cursor associated with that holder.
+         * It is null if the view is not associated with a cursor anymore.
+         */
+        public Cursor mCursor;
+
+        public void clearCursor() {
+            mCursor = null;
+        }
+        
+        public void setUiData(Cursor cursor) {
+            mCursor = cursor;
+        }
+    }
+
+    /**
+     * The holder for a profile header row.
+     */
+    private class ProfileHeaderHolder extends CursorHolder {
         
         public final CheckBox mCheckName;
 
         public ProfileHeaderHolder(Cursor cursor, View view) {
-            
             mCheckName = (CheckBox) view.findViewById(R.id.profileTitle);
         }
         
+        @Override
         public void setUiData(Cursor cursor) {
+            super.setUiData(cursor);
             mCheckName.setText(cursor.getString(mDescColIndex));
             mCheckName.setChecked(cursor.getInt(mEnableColIndex) != 0);
         }
     }
-    
-    private class TimedActionHolder {
+
+    /**
+     * The holder for a timed action row.
+     */
+    private class TimedActionHolder extends CursorHolder {
         
         public final TextView mDescription;
 
@@ -206,7 +331,9 @@ public class ProfilesUI extends Activity {
             mDescription = (TextView) view.findViewById(R.id.timedActionTitle);
         }
 
+        @Override
         public void setUiData(Cursor cursor) {
+            super.setUiData(cursor);
             mDescription.setText(cursor.getString(mDescColIndex));
             mDescription.setCompoundDrawablesWithIntrinsicBounds(
                     cursor.getInt(mEnableColIndex) == 0 ? mGrayDot : mGreenDot,
