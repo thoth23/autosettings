9e7d127e7412e7238b3dc32dc23ec4f061b33cf5
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-01-25 04:52:28 +0000

    profiles ui cleanup, remove obsolete list adapter.

--- AutoSettings/src/com/alfray/timeriffic/profiles/ProfilesUI.java
+++ AutoSettings/src/com/alfray/timeriffic/profiles/ProfilesUI.java
@@ -1,12 +1,10 @@
 /*
  * (c) ralfoide gmail com, 2009
  * Project: Timeriffic
- * License TBD
+ * License GPLv3
  */
 
 package com.alfray.timeriffic.profiles;
-
-import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Context;
@@ -26,7 +24,6 @@ import com.alfray.timeriffic.R;
 public class ProfilesUI extends Activity {
 
     private ListView mProfilesList;
-    private ArrayList<Profile> mProfiles;
     private ProfileCursorAdapter mAdapter;
     private LayoutInflater mLayoutInflater;
     private ProfilesDB mProfilesDb;
@@ -42,9 +39,6 @@ public class ProfilesUI extends Activity {
         setContentView(R.layout.profiles_screen);
         mLayoutInflater = getLayoutInflater();
         
-        mProfiles = new ArrayList<Profile>();
-        addDummyData();
-
         mProfilesList = (ListView) findViewById(R.id.profilesList);
         
         mProfilesDb = new ProfilesDB();
@@ -73,22 +67,8 @@ public class ProfilesUI extends Activity {
 
         mAdapter = new ProfileCursorAdapter(this, cursor);
         mProfilesList.setAdapter(mAdapter);
-        
-        /*
-        mAdapter = new ProfileListAdapter();
-        */
     }
 
-    private void addDummyData() {
-        Profile p = new Profile();
-        mProfiles.add(p);
-        p.getActions().add(new TimedAction(p));
-        p = new Profile();
-        mProfiles.add(p);
-        p.getActions().add(new TimedAction(p));
-        p.getActions().add(new TimedAction(p));
-    }
-    
     //--------------
 
     private class ProfileCursorAdapter extends CursorAdapter {
@@ -124,7 +104,7 @@ public class ProfilesUI extends Activity {
                 return TYPE_PROFILE;
             if (type == Columns.TYPE_IS_TIMED_ACTION)
                 return TYPE_TIMED_ACTION;
-            // throw?
+
             return IGNORE_ITEM_VIEW_TYPE;
         }
 
@@ -161,7 +141,6 @@ public class ProfilesUI extends Activity {
                 ProfileHeaderHolder h = (ProfileHeaderHolder) view.getTag();
                 h.setUiData(cursor);
             } else if (type == Columns.TYPE_IS_TIMED_ACTION) {
-                View v = mLayoutInflater.inflate(R.layout.timed_action, null);
                 TimedActionHolder h = ( TimedActionHolder) view.getTag();
                 h.setUiData(cursor);
             }
@@ -170,127 +149,9 @@ public class ProfilesUI extends Activity {
     
     //--------------
 
-    /*
-    private class ProfileListAdapter implements ListAdapter {
-
-        private final static int TYPE_PROFILE = 0;
-        private final static int TYPE_TIMED_ACTION = 1;
-        
-        private final HashSet<DataSetObserver> mObservers = new HashSet<DataSetObserver>();
-        
-        @Override
-        public boolean areAllItemsEnabled() {
-            return true;
-        }
-
-        @Override
-        public boolean isEnabled(int position) {
-            return true;
-        }
-
-        @Override
-        public int getCount() {
-            int n = 0;
-            for(Profile p : mProfiles) {
-                // header + number of actions
-                n += 1 + p.getActions().size();
-            }
-            return n;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            int n = 0;
-            for(Profile p : mProfiles) {
-                // is it the header?
-                if (n == position) return p;
-                n++;
-                for (TimedAction a : p.getActions()) {
-                    if (n == position) return a;
-                    n++;
-                }
-            }
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            Object o = getItem(position);
-            if (o instanceof TimedAction)
-                return ((TimedAction)o).getId();
-            if (o instanceof Profile)
-                return ((Profile)o).getId();
-            return 0;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return 2;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return mProfiles.size() == 0;
-        }
-
-        @Override
-        public void registerDataSetObserver(DataSetObserver observer) {
-            mObservers.add(observer);
-        }
-
-        @Override
-        public void unregisterDataSetObserver(DataSetObserver observer) {
-            mObservers.remove(observer);
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            Object o = getItem(position);
-            if (o instanceof TimedAction)
-                return TYPE_TIMED_ACTION;
-            if (o instanceof Profile)
-                return TYPE_PROFILE;
-            return IGNORE_ITEM_VIEW_TYPE;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            Object o = getItem(position);
-            View v = null;
-
-            if (o instanceof Profile) {
-                if (convertView == null ||
-                        !(convertView.getTag() instanceof ProfileHeaderHolder)) {
-                    v = mLayoutInflater.inflate(R.layout.profile_header, null);
-                    v.setTag(new ProfileHeaderHolder(v));
-                }
-                ((ProfileHeaderHolder)v.getTag()).setUiData((Profile) o);
-            }
-
-            if (o instanceof TimedAction) {
-                if (convertView == null ||
-                        !(convertView.getTag() instanceof TimedActionHolder)) {
-                    v = mLayoutInflater.inflate(R.layout.timed_action, null);
-                    v.setTag(new TimedActionHolder(v));
-                }
-                ((TimedActionHolder)v.getTag()).setUiData((TimedAction) o);
-            }
-            
-            return v;
-        }
-        
-    }
-    */
 
     private class ProfileHeaderHolder {
         
-        /** @deprecated */ public Profile mProfile;
-
         public final CheckBox mCheckName;
         public final ImageButton mButton;
 
@@ -300,12 +161,6 @@ public class ProfilesUI extends Activity {
             mButton = (ImageButton) view.findViewById(R.id.profileButton);
         }
         
-        public void setUiData(Profile profile) {
-            mProfile = profile;
-            mCheckName.setText(profile.getName());
-            mCheckName.setEnabled(profile.isEnabled());
-        }
-
         public void setUiData(Cursor cursor) {
             mCheckName.setText(cursor.getString(mDescColIndex));
             mCheckName.setChecked(cursor.getInt(mEnableColIndex) != 0);
@@ -314,17 +169,10 @@ public class ProfilesUI extends Activity {
     
     private class TimedActionHolder {
         
-        /** @deprecated */ public TimedAction mTimedAction;
-
         public final TextView mDescription;
 
         public TimedActionHolder(Cursor cursor, View view) {
             mDescription = (TextView) view.findViewById(R.id.timedActionTitle);
-        }
-
-        public void setUiData(TimedAction timedAction) {
-            mTimedAction = timedAction;
-            mDescription.setText(timedAction.toString());
         }
 
         public void setUiData(Cursor cursor) {
