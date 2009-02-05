9e47976cb293c43455c35cad2a246477fc4bad79
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-02-05 06:38:03 +0000

    action edit set ui

--- AutoSettings/src/com/alfray/timeriffic/profiles/Columns.java
+++ AutoSettings/src/com/alfray/timeriffic/profiles/Columns.java
@@ -89,19 +89,25 @@ public class Columns implements BaseColumns {
     public static final String HOUR_MIN = "hourMin";
 
     /** Day(s) of the week.
-     * This is a bitfield: {@link #MONDAY} thru {@link #SUNDAY}.
+     * This is a bitfield: {@link #MONDAY} thru {@link #SUNDAY} at
+     * bit indexes {@link #MONDAY_BIT_INDEX} thru {@link #SUNDAY_BIT_INDEX}.
      * <p/>
      * Type: INTEGER
      */
     public static final String DAYS = "days";
 
-    public static final int MONDAY    =  1;
-    public static final int TUESDAY   =  2;
-    public static final int WEDNESDAY =  4;
-    public static final int THURSDAY  =  8;
-    public static final int FRIDAY    = 16;
-    public static final int SATURDAY  = 32;
-    public static final int SUNDAY    = 64;
+    public static final int MONDAY_BIT_INDEX = 0;
+    public static final int SUNDAY_BIT_INDEX = 6;
+
+    public static final int MONDAY    = 1 << MONDAY_BIT_INDEX;
+    public static final int TUESDAY   = 1 << 1;
+    public static final int WEDNESDAY = 1 << 2;
+    public static final int THURSDAY  = 1 << 3;
+    public static final int FRIDAY    = 1 << 4;
+    public static final int SATURDAY  = 1 << 5;
+    public static final int SUNDAY    = 1 << SUNDAY_BIT_INDEX;
+
+
 
     /** Actions to execute.
      * This is an encoded string:
--- AutoSettings/src/com/alfray/timeriffic/profiles/EditActionUI.java
+++ AutoSettings/src/com/alfray/timeriffic/profiles/EditActionUI.java
@@ -15,6 +15,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 public class EditActionUI extends Activity {
 
@@ -23,6 +25,18 @@ public class EditActionUI extends Activity {
     /** Extra long with the action prof_id (not index) to edit. */
     public static final String EXTRA_ACTION_ID = "action_id";
     private long mActionId;
+
+    private TimePicker mTimePicker;
+
+    private ToggleButton mToggleRinger;
+
+    private ToggleButton mToggleVib;
+
+    /**
+     * Day checkboxes, in the same index order than {@link Columns#MONDAY_BIT_INDEX}
+     * to {@link Columns#SUNDAY_BIT_INDEX}.
+     */
+    private CheckBox[] mCheckDays;
 
     /** Called when the activity is first created. */
     @Override
@@ -43,39 +57,81 @@ public class EditActionUI extends Activity {
             return;
         }
         
-        String action_id_select = String.format("%s=%d",
-                Columns.PROFILE_ID, mActionId);
-        
+        // get profiles db helper
         ProfilesDB profilesDb = new ProfilesDB();
         profilesDb.onCreate(this);
+        
+        // get cursor
+        String prof_id_select = String.format("%s=%d", Columns.PROFILE_ID, mActionId);
         Cursor c = profilesDb.query(
                 -1, // id
+                // projection, a.k.a. the list of columns to retrieve from the db
                 new String[] {
                         Columns.PROFILE_ID,
-                        Columns.DESCRIPTION,
-                        Columns.IS_ENABLED
+                        Columns.HOUR_MIN,
+                        Columns.DAYS,
+                        Columns.ACTIONS
                 },
-                action_id_select, // selection
+                prof_id_select, // selection
                 null, // selectionArgs
                 null // sortOrder
                 );
         try {
-            /*
-            int descColIndex = c.getColumnIndexOrThrow(Columns.DESCRIPTION);
-            int enColIndex = c.getColumnIndexOrThrow(Columns.IS_ENABLED);
-
-            if (c.moveToFirst()) {
-                mNameField = (EditText) findViewById(R.id.name);
-                mEnabledCheck = (CheckBox) findViewById(R.id.enabled);
-                
-                mNameField.setText(c.getString(descColIndex));
-                mEnabledCheck.setChecked(c.getInt(enColIndex) != 0);
-            } else {
-                Log.e(TAG, "cursor is empty: " + action_id_select);
+            if (!c.moveToFirst()) {
+                Log.e(TAG, "cursor is empty: " + prof_id_select);
                 finish();
                 return;
             }
-            */
+
+            // get UI widgets
+            mTimePicker = (TimePicker) findViewById(R.id.timePicker);
+            mToggleRinger = (ToggleButton) findViewById(R.id.toggleRinger);
+            mToggleVib = (ToggleButton) findViewById(R.id.toggleVib);
+            mCheckDays = new CheckBox[] {
+                    (CheckBox) findViewById(R.id.dayMon),
+                    (CheckBox) findViewById(R.id.dayMon),
+                    (CheckBox) findViewById(R.id.dayTue),
+                    (CheckBox) findViewById(R.id.dayWed),
+                    (CheckBox) findViewById(R.id.dayThu),
+                    (CheckBox) findViewById(R.id.dayFri),
+                    (CheckBox) findViewById(R.id.daySat),
+                    (CheckBox) findViewById(R.id.daySun),
+            };
+            
+            // get column indexes
+            int hourMinColIndex = c.getColumnIndexOrThrow(Columns.HOUR_MIN);
+            int daysColIndex = c.getColumnIndexOrThrow(Columns.DAYS);
+            int actionsColIndex = c.getColumnIndexOrThrow(Columns.ACTIONS);
+
+            // fill in UI from cursor data
+            setTimePickerValue(mTimePicker, c.getInt(hourMinColIndex));
+            
+            int days = c.getInt(daysColIndex);
+            for (int i = Columns.MONDAY_BIT_INDEX; i <= Columns.SUNDAY_BIT_INDEX; i++) {
+                mCheckDays[i].setChecked((days & (1<<i)) != 0);
+            }
+            
+            String actions = c.getString(actionsColIndex);
+            for (String action : actions.split(",")) {
+                int value = -1;
+                if (action.length() > 1) {
+                    try {
+                        value = Integer.parseInt(action.substring(1));
+                    } catch (NumberFormatException e) {
+                        Log.e(TAG, String.format("Invalid action '%s' in '%s", action, actions));
+                        
+                    }
+                }
+                if (action.startsWith(Columns.ACTION_MUTE)) {
+                    mToggleRinger.setEnabled(value >= 0);
+                    mToggleRinger.setChecked(value == 0);
+                }
+                if (action.startsWith(Columns.ACTION_VIBRATE)) {
+                    mToggleVib.setEnabled(value >= 0);
+                    mToggleVib.setChecked(value > 0);
+                }
+            }
+            
         } finally {
             c.close();
             profilesDb.onDestroy();
@@ -99,4 +155,17 @@ public class EditActionUI extends Activity {
             profilesDb.onDestroy();
         }
     }
+    
+    // -----------
+
+    private void setTimePickerValue(TimePicker timePicker, int hourMin) {
+        if (hourMin < 0) hourMin = 0;
+        if (hourMin >= 24*60) hourMin = 24*60-1;
+        int hours = hourMin / 60;
+        int minutes = hourMin % 60;
+        
+        timePicker.setCurrentHour(hours);
+        timePicker.setCurrentMinute(minutes);
+    }
+
 }
