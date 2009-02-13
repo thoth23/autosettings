74856553e4abb33d27e05ce397f65f7e826b3494
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-02-13 02:53:36 +0000

    Remove old start/top prefs & update code

--- AutoSettings/src/com/alfray/timeriffic/prefs/PrefsValues.java
+++ AutoSettings/src/com/alfray/timeriffic/prefs/PrefsValues.java
@@ -9,13 +9,9 @@ package com.alfray.timeriffic.prefs;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
 
 public class PrefsValues {
-
-    public static final String KEY_START_HOUR = "start_hour";
-    public static final String KEY_END_HOUR = "end_hour";
 
     private SharedPreferences mPrefs;
 	
@@ -49,60 +45,5 @@ public class PrefsValues {
      */
     public boolean setIntroDismissed(boolean dismiss) {
         return mPrefs.edit().putBoolean("dismiss_intro", dismiss).commit();
-    }
-
-    public int startHourMin() {
-        try {
-            return mPrefs.getInt(KEY_START_HOUR, 10*60);
-        } catch (ClassCastException e) {
-            // The field used to be a String, so it could fail here
-            String s = mPrefs.getString(KEY_START_HOUR, "10");
-            return TimePreference.parseHoursMin(s);
-        }
-    }
-
-    public int stopHourMin() {
-        try {
-            return mPrefs.getInt(KEY_END_HOUR, 14*60);
-        } catch (ClassCastException e) {
-            // The field used to be a String, so it could fail here
-            String s = mPrefs.getString(KEY_END_HOUR, "14");
-            return TimePreference.parseHoursMin(s);
-        }
-    }
-
-    public boolean startMute() {
-        return mPrefs.getBoolean("start_mute", true);
-    }
-
-    public boolean startVibrate() {
-        return mPrefs.getBoolean("start_vibrate", true);
-    }
-
-    public boolean stopMute() {
-        return mPrefs.getBoolean("end_mute", false);
-    }
-
-    public boolean stopVibrate() {
-        return mPrefs.getBoolean("end_vibrate", false);
-    }
-    
-    public String getLog() {
-        return mPrefs.getString("log", "");
-    }
-    
-    public void appendToLog(String msg) {
-        long now = System.currentTimeMillis();
-        msg = String.format("%d:%s\n", now, msg);
-        
-        String current = mPrefs.getString("log", "");
-        
-        Editor editor = mPrefs.edit();
-        
-        try {
-            editor.putString("log", current + msg);
-        } finally {
-            editor.commit();
-        }
     }
 }
