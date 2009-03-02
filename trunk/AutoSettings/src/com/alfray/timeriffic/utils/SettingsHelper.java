d6f2ff18aa13619608cb2411774fd7aeaed5ddce
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-03-02 06:26:41 +0000

    change brightness

--- trunk/AutoSettings/src/com/alfray/timeriffic/utils/SettingsHelper.java
+++ trunk/AutoSettings/src/com/alfray/timeriffic/utils/SettingsHelper.java
@@ -9,6 +9,8 @@ package com.alfray.timeriffic.utils;
 import android.content.Context;
 import android.media.AudioManager;
 import android.net.wifi.WifiManager;
+import android.os.IHardwareService;
+import android.os.ServiceManager;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
@@ -108,6 +110,15 @@ public class SettingsHelper {
         int vol = (max * percent) / 100;
         manager.setStreamVolume(AudioManager.STREAM_RING, vol, 0 /*flags*/);
     }
+    
+    public int getRingerVolume() {
+        AudioManager manager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+
+        int vol = manager.getStreamVolume(AudioManager.STREAM_RING);
+        int max = manager.getStreamMaxVolume(AudioManager.STREAM_RING);
+        
+        return (vol * 100 / max);
+    }
 
     public void changeWifi(boolean enabled) {
         WifiManager manager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
@@ -117,25 +128,41 @@ public class SettingsHelper {
         manager.setWifiEnabled(enabled);
     }
     
-    private static int MIN_BRIGHTNESS = 10;
-    private static int MAX_BRIGHTNESS = 255;
-    
-    public void changeBrightness(int percent) {
+    private static int MIN_BRIGHTNESS = android.os.Power.BRIGHTNESS_DIM + 10;
+    private static int MAX_BRIGHTNESS = android.os.Power.BRIGHTNESS_ON;
+
+    /**
+     * @param percent The new value in 0..100 range (will get mapped to adequate OS values)
+     * @param persistent True if the setting should be made persistent, e.g. written to system pref.
+     *  If false, only the current hardware value is changed.
+     */
+    public void changeBrightness(int percent, boolean persistent) {
         // Reference:
         // http://android.git.kernel.org/?p=platform/packages/apps/Settings.git;a=blob;f=src/com/android/settings/BrightnessPreference.java
         // The source indicates
         // - Backlight range is 0..255
         // - Must not set to 0 (user would see nothing) so they use 10 as minimum
-        // - All constants are in android.os.Power which is hidden from the SDK
+        // - All constants are in android.os.Power which is hidden from the SDK in 1.0
+        //   yet available in 1.1
         // - To get value: Settings.System.getInt(getContext().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS);
         // - To set value: Settings.System.putInt(getContext().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, v);
 
         Log.d(TAG, "changeBrightness: " + Integer.toString(percent));
-
+        
         int v = MIN_BRIGHTNESS + percent * (MAX_BRIGHTNESS - MIN_BRIGHTNESS) / 100;
-        Settings.System.putInt(mContext.getContentResolver(),
-                Settings.System.SCREEN_BRIGHTNESS,
-                v);
+        
+        if (persistent) {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS,
+                    v);
+        }
+        
+        try {
+            IHardwareService hs = IHardwareService.Stub.asInterface(ServiceManager.getService("hardware"));
+            if (hs != null) hs.setScreenBacklight(v);
+        } catch (Throwable t) {
+            Log.e(TAG, "Failed to set brightness to " + Integer.toString(v), t);
+        }
     }
 
     /**
