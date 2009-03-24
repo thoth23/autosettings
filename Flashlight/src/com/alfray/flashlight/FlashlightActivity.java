9c875f016834677a4b3192d1d1be6a0f4d0b0086
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-03-24 22:17:45 +0000

    Brightlight vs darklight

--- Flashlight/src/com/alfray/flashlight/FlashlightActivity.java
+++ Flashlight/src/com/alfray/flashlight/FlashlightActivity.java
@@ -1,17 +1,73 @@
 package com.alfray.flashlight;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
-public class FlashlightActivity extends Activity {
+public abstract class FlashlightActivity extends Activity {
 
+    private final String mTag;
+
+    public FlashlightActivity(String tag) {
+        mTag = tag;
+    }
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        findViewById(R.id.GoBright).setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(FlashlightActivity.this, BrightlightActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        findViewById(R.id.GoDark).setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(FlashlightActivity.this, DarklightActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        TextView tv = (TextView) findViewById(R.id.Label);
+        initializeOnCreate(tv);
+        Log.d(mTag, "onCreate");
+    }
+
+    protected abstract void initializeOnCreate(TextView tv);
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(mTag, "onResume");
+    }
+
+    @Override
+    protected void onUserLeaveHint() {
+        super.onUserLeaveHint();
+
+        Log.d(mTag, "onUserLeaveHint");
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(mTag, "onPause");
+    }
+
+    protected void setBrightness(float value) {
         // Make the screen full bright for this activity using the new
         // cupcake brightness API.
         //
@@ -21,12 +77,8 @@ public class FlashlightActivity extends Activity {
         //   http://android.git.kernel.org/?p=platform/development.git;a=blob;f=apps/Development/src/com/android/development/PointerLocation.java;h=668e9ba167f590c97481e348ced5f97d45f307c9;hb=HEAD
 
         WindowManager.LayoutParams lp = getWindow().getAttributes();
-        lp.screenBrightness = 1.0f;
+        lp.screenBrightness = value;
         getWindow().setAttributes(lp);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
+        Log.d(mTag, "Set brightness to " + Float.toString(value));
     }
 }
