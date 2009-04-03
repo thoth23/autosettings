3322540aea881cd03883f2b035e87111dc053c76
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-04-03 06:27:28 +0000

    Ability to change flashlight color

--- Flashlight/src/com/alfray/flashlight/FlashlightActivity.java
+++ Flashlight/src/com/alfray/flashlight/FlashlightActivity.java
@@ -7,7 +7,13 @@
 package com.alfray.flashlight;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -22,6 +28,30 @@ public abstract class FlashlightActivity extends Activity {
 
     private final String mTag;
 
+    private PrefsValues mPrefs;
+
+    private static final String sColorNames[] = {
+        "White",
+        "Red",
+        "Orange",
+        "Yellow",
+        "Green",
+        "Cyan",
+        "Blue",
+        "Purple"
+    };
+
+    private static final int sColorHex[] = {
+        0xFFFFFFFF,       // white
+        0xFFFF0000,       // red
+        0xFFFF9000,       // orange
+        0xFFFFFF00,       // yellow
+        0xFF00FF00,       // green
+        0xFF00FFFF,       // cyan
+        0xFF0000FF,       // blue
+        0xFFFF00FF        // purple
+    };
+
     public FlashlightActivity(String tag) {
         mTag = tag;
     }
@@ -31,6 +61,9 @@ public abstract class FlashlightActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mPrefs = new PrefsValues(this);
+        applyColor(mPrefs.getColorIndex());
 
         findViewById(R.id.GoBright).setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
@@ -95,6 +128,7 @@ public abstract class FlashlightActivity extends Activity {
         menu.add(0, R.string.menu_bright, 0, R.string.menu_bright).setIcon(R.drawable.bright_icon);
         menu.add(0, R.string.menu_dark  , 0, R.string.menu_dark  ).setIcon(R.drawable.dark_icon);
         menu.add(0, R.string.menu_about , 0, R.string.menu_about ).setIcon(R.drawable.ic_menu_help);
+        menu.add(0, R.string.menu_color , 0, R.string.menu_color ).setIcon(R.drawable.color_icon);
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -112,6 +146,9 @@ public abstract class FlashlightActivity extends Activity {
             Intent i = new Intent(this, AboutActivity.class);
             startActivity(i);
             break;
+        case R.string.menu_color:
+            showDialog(R.string.menu_color);
+            break;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -126,5 +163,39 @@ public abstract class FlashlightActivity extends Activity {
         Intent i = new Intent(this, DarklightActivity.class);
         startActivity(i);
         finish();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(final int id) {
+
+        if (id == R.string.menu_color) {
+            Builder b = new AlertDialog.Builder(this);
+
+            b.setTitle("Choose color");
+
+            b.setSingleChoiceItems(sColorNames,
+                    mPrefs.getColorIndex(),
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            changeColor(which);
+                            dismissDialog(R.string.menu_color);
+                        }
+            });
+
+            return b.create();
+        }
+
+        return super.onCreateDialog(id);
+    }
+
+    private void changeColor(int colorIndex) {
+        mPrefs.setColorIndex(colorIndex);
+        applyColor(colorIndex);
+    }
+
+    private void applyColor(int colorIndex) {
+        if (colorIndex < 0 || colorIndex >= sColorHex.length) colorIndex = 0;
+        getWindow().setBackgroundDrawable(new ColorDrawable(sColorHex[colorIndex]));
     }
 }
