500c4571fb607f513f51a3848505cfc612afd859
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-04-05 03:06:58 +0000

    Update TAGs to be more readable.

---
+++ trunk/AutoSettings/src/com/alfray/timeriffic/prefs/Oldv1PrefsValues.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2008 (c) ralfoide gmail com, 2008
+ * Project: auto settings
+ * License: GPL version 3 or any later version
+ */
+
+package com.alfray.timeriffic.prefs;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class Oldv1PrefsValues {
+
+    public static final String KEY_START_HOUR = "start_hour";
+    public static final String KEY_END_HOUR = "end_hour";
+
+    private SharedPreferences mPrefs;
+    
+    public Oldv1PrefsValues(Context context) {
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+    
+    public SharedPreferences getPrefs() {
+        return mPrefs;
+    }
+
+    public boolean isServiceEnabled() {
+        return mPrefs.getBoolean("enable_serv", true);
+    }
+    
+    /**
+     * Sets the dismiss_intro boolean value.
+     * @return true if value was successfully changed if the prefs
+     */
+    public boolean setServiceEnabled(boolean checked) {
+        return mPrefs.edit().putBoolean("enable_serv", checked).commit();
+    }
+
+    public int startHourMin() {
+        try {
+            return mPrefs.getInt(KEY_START_HOUR, 10*60);
+        } catch (ClassCastException e) {
+            // The field used to be a String, so it could fail here
+            String s = mPrefs.getString(KEY_START_HOUR, "10");
+            return parseHoursMin(s);
+        }
+    }
+
+    public int stopHourMin() {
+        try {
+            return mPrefs.getInt(KEY_END_HOUR, 14*60);
+        } catch (ClassCastException e) {
+            // The field used to be a String, so it could fail here
+            String s = mPrefs.getString(KEY_END_HOUR, "14");
+            return parseHoursMin(s);
+        }
+    }
+
+    private int parseHoursMin(String text) {
+        int hours = 0;
+        int minutes = 0;
+        
+        String[] numbers = text.trim().split(":");
+        if (numbers.length >= 1) hours = parseNumber(numbers[0], 23);
+        if (numbers.length >= 2) minutes = parseNumber(numbers[1], 59);
+
+        return hours*60 + minutes;
+    }
+
+    private static int parseNumber(String string, int maxValue) {
+        try {
+            int n = Integer.parseInt(string);
+            if (n < 0) return 0;
+            if (n > maxValue) return maxValue;
+            return n;
+        } catch (Exception e) {
+            // ignore
+        }
+        return 0;
+    }
+    
+    public boolean startMute() {
+        return mPrefs.getBoolean("start_mute", true);
+    }
+
+    public boolean startVibrate() {
+        return mPrefs.getBoolean("start_vibrate", true);
+    }
+
+    public boolean stopMute() {
+        return mPrefs.getBoolean("end_mute", false);
+    }
+
+    public boolean stopVibrate() {
+        return mPrefs.getBoolean("end_vibrate", false);
+    }
+}
