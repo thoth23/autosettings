66e981dd4f658c4dd10f57775197e3ae74f5ff2f
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-04-15 08:44:06 +0000

    Initial skeleton app

---
+++ Brighteriffic/dummy/com/alfray/brighteriffic/ServiceManager.java
@@ -0,0 +1,25 @@
+/*
+ * (c) ralfoide gmail com, 2009
+ * Project: Brighteriffic
+ * License TBD
+ */
+
+/**
+ *
+ */
+package com.alfray.brighteriffic;
+
+import android.os.IBinder;
+
+/**
+ * @author ralf
+ *
+ */
+public class ServiceManager {
+
+    public static IBinder getService(String string) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
