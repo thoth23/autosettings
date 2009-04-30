4c438d842285886d67080046309e8c1430404565
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-04-30 06:53:05 +0000

    About box link to visit project page

--- Flashlight/src/com/alfray/flashlight/AboutActivity.java
+++ Flashlight/src/com/alfray/flashlight/AboutActivity.java
@@ -6,8 +6,15 @@
 
 package com.alfray.flashlight;
 
+import java.net.URL;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class AboutActivity extends Activity {
 
@@ -16,6 +23,17 @@ public class AboutActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+
+        Button goProject = (Button) findViewById(R.id.GoProject);
+        goProject.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://code.google.com/p/autosettings/wiki/Flashlight"));
+                startActivity(intent);
+            }
+        });
     }
 
 }
