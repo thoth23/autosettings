ba3ba01aa1288da6f21cff6c93d12646138d4761
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-06-07 04:50:22 +0000

    Testing

---
+++ Brighteriffic/src/com/android/internal/os/BinderInternal.java
@@ -0,0 +1,25 @@
+/*
+ * (c) ralfoide gmail com, 2008
+ * Project: Brighteriffic
+ * License TBD
+ */
+
+package com.android.internal.os;
+
+import android.os.IBinder;
+
+//-----------------------------------------------
+
+/**
+ *  Stub
+ */
+public class BinderInternal {
+
+    /** Stub */
+    public static IBinder getContextObject() {
+        return null;
+    }
+
+}
+
+
