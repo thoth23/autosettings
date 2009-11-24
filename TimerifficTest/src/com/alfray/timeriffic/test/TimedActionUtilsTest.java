f7c162f98d1247add0effe11894b81937f996e8c
ralfoide <ralfoide@d6266d86-a85f-11dd-9c66-c9f8973bfffa>
2009-11-24 07:00:03 +0000

    Simple test for day names.

---
+++ TimerifficTest/src/com/alfray/timeriffic/test/TimedActionUtilsTest.java
@@ -0,0 +1,75 @@
+/*
+ * (c) ralfoide gmail com, 2009
+ * Project: TimerifficTest
+ * License GPLv3
+ */
+
+/**
+ *
+ */
+package com.alfray.timeriffic.test;
+
+import java.util.Locale;
+
+import com.alfray.timeriffic.actions.TimedActionUtils;
+
+import junit.framework.TestCase;
+
+public class TimedActionUtilsTest extends TestCase {
+
+    private static class MockTimedActionUtils extends TimedActionUtils {
+
+        public String[] _getDaysNames() {
+            TimedActionUtils.sDaysNames = null;
+            return TimedActionUtils.getDaysNames();
+        }
+
+    }
+
+    private MockTimedActionUtils m;
+    private Locale mDefaultLocale;
+
+    @Override
+    protected void setUp() throws Exception {
+        mDefaultLocale = Locale.getDefault();
+        m = new MockTimedActionUtils();
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        Locale.setDefault(mDefaultLocale);
+        m = null;
+        super.tearDown();
+    }
+
+    public void testInitDaysNames_en() throws Exception {
+
+        Locale.setDefault(new Locale("en"));
+
+        String[] days = m._getDaysNames();
+        assertNotNull(days);
+        assertEquals(7, days.length);
+
+        String[] expected = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+
+        for (int i = 0; i < 7; i++) {
+            assertEquals(expected[i], days[i]);
+        }
+    }
+
+    public void testInitDaysNames_fr() throws Exception {
+
+        Locale.setDefault(new Locale("fr"));
+
+        String[] days = m._getDaysNames();
+        assertNotNull(days);
+        assertEquals(7, days.length);
+
+        String[] expected = { "lun.", "mar.", "mer.", "jeu.", "ven.", "sam.", "dim." };
+
+        for (int i = 0; i < 7; i++) {
+            assertEquals(expected[i], days[i]);
+        }
+    }
+}
